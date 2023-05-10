80c630dffeecf8081b4ca1eb860cb8aaf854bfdd
zero-penguin <arato1060@outlook.jp>
2023-05-10 18:23:39 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+$(function(){
+	
+	var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+	
+	if (winW > 880){
+		var headerH =20;
+	}
+	else{
+		var headerH =60;
+	}
+	
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+	
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+
+});
+
+$(function () {
+  // ボタンアニメーション
+  $('.button-more').on('mouseover', function () {
+    $(this).animate({
+      opacity: 0.5,
+      marginLeft: 20,
+    }, 100);
+  });
+  $('.button-more').on('mouseout', function () {
+    $(this).animate({
+      opacity: 1.0,
+      marginLeft: 0
+    }, 100);
+  });
+	// カルーセル
+	$('.carousel').slick({
+		autoplay: true,
+		dots: true,
+		infinite: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+	});
+});
